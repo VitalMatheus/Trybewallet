b6b7cf86f3c845f937c5466de99add18a8ad4bba
Matheus Vital Diniz de Souza <matheus@localhost.localdomain>
2022-04-01 10:53:22 -0300

    Add third requirement

---
+++ src/pages/Header.js
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
+
+class Header extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      expenses: 0,
+      exchange: 'BRL',
+    };
+  }
+
+  render() {
+    const { expenses, exchange } = this.state;
+    const { recoveredEmail } = this.props;
+    return (
+      <div>
+        <h4 data-testid="email-field">{ recoveredEmail }</h4>
+        <h4 data-testid="total-field">{ expenses }</h4>
+        <h4 data-testid="header-currency-field">{ exchange }</h4>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  recoveredEmail: state.user.email,
+});
+
+Header.propTypes = ({
+  recoveredEmail: PropTypes.string.isRequired,
+});
+
+export default connect(mapStateToProps, null)(Header);
